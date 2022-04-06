003b039061a3486eb005a8d4251bcf2fc9ec0457
z_one_y <1224503495@qq.com>
2022-04-06 16:28:08 +0800

    添加Jenkinsfile

--- demo2/src/main/java/me/zoney/demo2/mq/SpringRabbitListener.java
+++ demo2/src/main/java/me/zoney/demo2/mq/SpringRabbitListener.java
@@ -15,30 +15,30 @@ public class SpringRabbitListener {
 //    public void listenSimpleQueueMessage(String msg) throws InterruptedException {
 //        System.out.println("spring 消费者接收到消息：【" + msg + "】");
 //    }
-    @RabbitListener(queues = "meter.queue")
-    public void listenSimpleQueueMessage2(Map<String,Object> msg) throws InterruptedException {
-        System.out.println("spring 消费者接收到消息2：【" + msg + "】");
-    }
-    @RabbitListener(queues = "dma.queue")
-    public void listenSimpleQueueMessage3(Map<String,Object> msg) throws InterruptedException {
-        System.out.println("spring 消费者接收到消息3：【" + msg + "】");
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(name = "topic.queue1"),
-            exchange = @Exchange(name = "metter.topic", type = ExchangeTypes.TOPIC),
-            key = "china.#"
-    ))
-    public void listenTopicQueue1(String msg){
-        System.out.println("消费者接收到topic.queue1的消息：【" + msg + "】");
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(name = "topic.queue2"),
-            exchange = @Exchange(name = "metter.topic", type = ExchangeTypes.TOPIC),
-            key = "#.news"
-    ))
-    public void listenTopicQueue2(String msg){
-        System.out.println("消费者接收到topic.queue2的消息：【" + msg + "】");
-    }
+//    @RabbitListener(queues = "meter.queue")
+//    public void listenSimpleQueueMessage2(Map<String,Object> msg) throws InterruptedException {
+//        System.out.println("spring 消费者接收到消息2：【" + msg + "】");
+//    }
+//    @RabbitListener(queues = "dma.queue")
+//    public void listenSimpleQueueMessage3(Map<String,Object> msg) throws InterruptedException {
+//        System.out.println("spring 消费者接收到消息3：【" + msg + "】");
+//    }
+//
+//    @RabbitListener(bindings = @QueueBinding(
+//            value = @Queue(name = "topic.queue1"),
+//            exchange = @Exchange(name = "metter.topic", type = ExchangeTypes.TOPIC),
+//            key = "china.#"
+//    ))
+//    public void listenTopicQueue1(String msg){
+//        System.out.println("消费者接收到topic.queue1的消息：【" + msg + "】");
+//    }
+//
+//    @RabbitListener(bindings = @QueueBinding(
+//            value = @Queue(name = "topic.queue2"),
+//            exchange = @Exchange(name = "metter.topic", type = ExchangeTypes.TOPIC),
+//            key = "#.news"
+//    ))
+//    public void listenTopicQueue2(String msg){
+//        System.out.println("消费者接收到topic.queue2的消息：【" + msg + "】");
+//    }
 }
